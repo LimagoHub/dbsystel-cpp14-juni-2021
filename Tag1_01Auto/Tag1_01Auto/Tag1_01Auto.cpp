7dc5735c34268ab5726ff9ed3a0a2e3a63034d78
Jo Wagner <jowagner@limago.de>
2021-06-07 09:31:05 +0200

    decltype

--- Tag1_01Auto/Tag1_01Auto/Tag1_01Auto.cpp
+++ Tag1_01Auto/Tag1_01Auto/Tag1_01Auto.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
-int main() 
+
+auto foo() noexcept ->int
 {
-	std::cout << "Hello World!\n";
+	return 42;
+}
+
+
+template<typename T1, typename T2>
+auto add(T1 t1, T2 t2)-> decltype(t1 + t2)
+{
+	return t1 + t2;
+}
+
+
+auto main() noexcept -> int
+{
+	auto ergebnis = add(1, 1.0);
+	
+	auto a = 10.0;
+	
+	auto b = a - 2;
+
+	auto* ptr = &a;
+
+	
 	
 }
 
