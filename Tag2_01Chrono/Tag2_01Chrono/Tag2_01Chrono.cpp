9430cbb33e944b640ea02dffdbc69a668dde16c5
Jo Wagner <jowagner@limago.de>
2021-06-08 09:28:47 +0200

    zeit

--- Tag2_01Chrono/Tag2_01Chrono/Tag2_01Chrono.cpp
+++ Tag2_01Chrono/Tag2_01Chrono/Tag2_01Chrono.cpp
@@ -1,10 +1,30 @@
 // Tag2_01Chrono.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
 //
 
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();  // Zeitpunkt
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+
+	std::chrono::duration<double, std::ratio<1>> dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	
+    std::cout << "Hello World!, Dauer: " << dur.count() << std::endl;
 }
 
+/*
+ *
+ typedef duration<signed int, nano> nanoseconds;
+typedef duration<signed int, micro> microseconds;
+typedef duration<signed int, milli> milliseconds;
+typedef duration<signed int> seconds;
+typedef duration<signed int, ratio< 60>> minutes;
+typedef duration<signed int, ratio<3600>> hours;
+ *
+ *
+ * 
+ */
